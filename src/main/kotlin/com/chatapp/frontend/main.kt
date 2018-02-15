f5e768583559ba9ce523b25f7f2a35c1149af08b
Jan Lipničan <jan.lipnican@cognito.cz>
2018-02-15 17:59:45 +0100

    something?

--- src/main/kotlin/com/chatapp/frontend/main.kt
+++ src/main/kotlin/com/chatapp/frontend/main.kt
@@ -1,5 +1,25 @@
 package com.chatapp.frontend
 
+import com.brianegan.bansa.*
+
+data class State(val count: Int = 0)
+
+class MainReducer() : Reducer<State> {
+
+    object INCREMENT: Action
+    object DECREMENT: Action
+
+    override fun reduce(state: State, action: Action): State {
+        return when (action) {
+            INCREMENT -> state.copy(count = state.count + 1)0
+            DECREMENT -> state.copy(count = state.count - 1)
+            else -> state
+        }
+    }
+}
+
 fun main(args: Array<String>) {
-    println("Hello klaudia!")
+
+    val counterStore = BaseStore(State(), MainReducer());
+
 }
